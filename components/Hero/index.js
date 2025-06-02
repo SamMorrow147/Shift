7466a04707e92e4d3be48ce8f1dae248cd6038d9
Sam Morrow <sammorrow@Sams-MacBook-Pro-3.local>
2025-06-02 14:37:41 -0500

    Add mobile-responsive video hero layout - video full width with titles below on mobile

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -7,13 +7,13 @@ class Hero extends Component {
         return(
             <section id="home" className="hero hero-slider-wrapper hero-style-1">
                 <div className="hero-slider">
-                    <div className="slide">
+                    <div className="slide mobile-video-slide">
                         <video 
                             autoPlay 
                             muted 
                             loop 
                             playsInline
-                            className="background-video"
+                            className="background-video mobile-full-video"
                             style={{
                                 position: 'absolute',
                                 width: '100%',
@@ -26,13 +26,13 @@ class Hero extends Component {
                         >
                             <source src="/ultra-compressed_765.mp4" type="video/mp4" />
                         </video>
-                        <div className="container">
+                        <div className="container mobile-content-container">
                             <div className="row">
-                                <div className="col col-md-10 col-sm-12 slide-caption">
-                                    <div className="slide-subtitle">
+                                <div className="col col-md-10 col-sm-12 slide-caption mobile-slide-caption">
+                                    <div className="slide-subtitle mobile-subtitle">
                                         <h4>Creative Design Agency</h4>
                                     </div>
-                                    <div className="slide-title">
+                                    <div className="slide-title mobile-title">
                                         <h2>Shift Creative</h2>
                                     </div>
                                 </div>
@@ -40,7 +40,7 @@ class Hero extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="social-links">
+                <div className="social-links mobile-social">
                     <div className="overlay"></div>
                     <ul>
                         <li><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/"><i className="fa fa-facebook"></i></a></li>
@@ -49,7 +49,7 @@ class Hero extends Component {
                         <li><a target="_blank" rel="noopener noreferrer" href="https://www.pinterest.com/"><i className="fa fa-pinterest"></i></a></li>
                     </ul>
                 </div>
-                <div className="white_svg">
+                <div className="white_svg mobile-svg">
                     <svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
                         <polygon points="0,300 655.167,210.5 1432.5,300 1920,198.5 1920,300 "></polygon>
                     </svg>
