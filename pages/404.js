fd261c06fde777d9caf29bdf0f3a6f661a8e247b
Sam Morrow <sammorrow@Sams-MacBook-Pro-3.local>
2025-06-02 14:04:29 -0500

    Fix 404 page image path - correct case sensitivity for Images directory

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import erimg from '/public/images/error-404.png'
+import erimg from '/public/Images/error-404.png'
 import Image from 'next/image'
 
 
@@ -20,7 +20,7 @@ const Error = (props) => {
                             </div>
                             <div className="error-message">
                                 <h3>Oops! Page Not Found!</h3>
-                                <p>We’re sorry but we can’t seem to find the page you requested. This might be because you have typed the web address incorrectly.</p>
+                                <p>We're sorry but we can't seem to find the page you requested. This might be because you have typed the web address incorrectly.</p>
                                 <Link onClick={ClickHandler} href="/" className="template-btn"> Back to home</Link>
                             </div>
                         </div>
